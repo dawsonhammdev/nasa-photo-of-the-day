92d75d60331e44442ea0da8765c567f8c77a212e
dawsonhammdev <hammsdesigns@gmail.com>
2020-03-19 17:16:27 -0500

    I am setting the axios up and trying to get the api working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+
+const [image, setimages] = useState([]);
 
 function App() {
   return (
@@ -11,5 +14,17 @@ function App() {
     </div>
   );
 }
+
+useEffect(() => {
+  axios
+    .get(`https://api.nasa.gov/planetary/apod?api_key=6cODF9iZMk6W5OAjZSXJBp2aTsjYTdMddz8NMvXM`)
+    .then(response => {
+      console.log(response)
+      setImage(response.data.image);
+    })
+    .catch(error => {
+      console.log("no pictures", error);
+    });
+}, []);
 
 export default App;
